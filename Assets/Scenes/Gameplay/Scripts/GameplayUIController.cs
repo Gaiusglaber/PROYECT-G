9a00c5fc54d64236ea0bb94a444045a655e74f91
Lucas Rodriguez Roge <lucas.rodriguez.roge@gmail.com>
2022-04-10 18:34:01 -0300

    Added URP,PP,UI,2D Lights and more.

--- Assets/Scenes/Gameplay/Scripts/GameplayUIController.cs
+++ Assets/Scenes/Gameplay/Scripts/GameplayUIController.cs
@@ -1,18 +1,90 @@
 using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Rendering;
+using UnityEngine.Rendering.Universal;
+using Pathfinders.Toolbox.Lerpers;
+using ProyectG.Gameplay.Objects;
 
-public class GameplayUIController : MonoBehaviour
+namespace ProyectG.Gameplay.UI
 {
-    // Start is called before the first frame update
-    void Start()
+    public class GameplayUIController : MonoBehaviour
     {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+        #region EXPOSED_FIELDS
+        [SerializeField] private Machine machine = null;
+        [Space(10)]
+        [Header("UI Data")]
+        [SerializeField] private TMPro.TMP_Text jewel0value = null;
+        [SerializeField] private TMPro.TMP_Text jewel1value = null;
+        [SerializeField] private TMPro.TMP_Text jewel2value = null;
+        [SerializeField] private TMPro.TMP_Text jewel3value = null;
+        [SerializeField] private TMPro.TMP_Text jewel4value = null;
+        [SerializeField] private TMPro.TMP_Text coinvalue = null;
+        [SerializeField] private float panelLerperSpeed = 0;
+        [SerializeField] private Vector2 posToLerpPanel = Vector2.zero;
+        [SerializeField] private GameObject panel = null;
+        [SerializeField] private Volume volume = null;
+        #endregion
+        #region PRIVATE_FIELDS
+        private DepthOfField dof = null;
+        private bool panelHidden = true;
+        private Vector2Lerper panelLerper = null;
+        private Vector2 initialPos = Vector2.zero;
+        private float initialdof = 0;
+        #endregion
+        #region UNTIY_CALLS
+        private void Start()
+        {
+            machine.OnInteract += ShowPanel;
+            volume.profile.TryGet(out dof);
+            initialPos = panel.GetComponent<RectTransform>().anchoredPosition;
+            initialdof = dof.focusDistance.value;
+            panelLerper = new Vector2Lerper(panelLerperSpeed, AbstractLerper<Vector2>.SMOOTH_TYPE.EASE_OUT);
+        }
+        #endregion
+        #region PUBLIC_METHODS
+        public void ShowPanel()
+        {
+            panelHidden = !panelHidden;
+            if (!panelHidden)
+            {
+                StartCoroutine(LerpPanel(posToLerpPanel));
+                StartCoroutine(LerpVolumeAttribute(dof, 0));
+            }
+            else
+            {
+                StartCoroutine(LerpPanel(initialPos));
+                StartCoroutine(LerpVolumeAttribute(dof, initialdof));
+            }
+        }
+        #endregion
+        #region PRIVATE_METHODS
+        #endregion
+        #region PUBLIC_CORROUTINES
+        #endregion
+        #region PRIVATE_CORROUTINES
+        private IEnumerator LerpPanel(Vector2 destPos)
+        {
+            RectTransform panelPosition = panel.GetComponent<RectTransform>();
+            panelLerper.SetValues(panelPosition.anchoredPosition, destPos, true);
+            while (panelLerper.On)
+            {
+                panelLerper.Update();
+                panelPosition.anchoredPosition = panelLerper.CurrentValue;
+                yield return new WaitForEndOfFrame();
+            }
+        }
+        private IEnumerator LerpVolumeAttribute(DepthOfField component,float destIntensity)
+        {
+            FloatLerper lerper = new FloatLerper(2, AbstractLerper<float>.SMOOTH_TYPE.STEP_SMOOTHER);
+            lerper.SetValues(component.focusDistance.value, destIntensity, true);
+            while (lerper.On)
+            {
+                lerper.Update();
+                component.focusDistance.value = lerper.CurrentValue;
+                yield return new WaitForEndOfFrame();
+            }
+        }
+        #endregion
     }
 }
---
+++ Assets/Scenes/Gameplay/Scripts/Machine.cs
@@ -0,0 +1,45 @@
+using System;
+using UnityEngine;
+
+namespace ProyectG.Gameplay.Objects{
+
+	#region CLASSES
+	public class Machine : MonoBehaviour
+	{
+
+		#region EXPOSED_FIELDS
+		#endregion
+
+		#region PRIVATE_FIELDS
+		public Action OnInteract = null;
+        #endregion
+
+        #region PROPERTIES
+        #endregion
+
+        #region UNITY_CALLS
+        private void Update()
+        {
+            if (Input.GetKeyDown(KeyCode.E))
+            {
+                OnInteract?.Invoke();
+            }
+        }
+        #endregion
+
+        #region PUBLIC_METHODS
+        #endregion
+
+        #region PRIVATE_METHODS
+        #endregion
+
+        #region PUBLIC_CORROUTINES
+        #endregion
+
+        #region PRIVATE_CORROUTINES
+        #endregion
+
+    }
+	#endregion
+
+}
